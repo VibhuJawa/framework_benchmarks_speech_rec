8b64945975b19020653a8f082a22cded2ee55b52
Giacomo Zavolta Taylor <gzt@GZTMBP.local>
2018-09-13 18:35:19 -0400

    edited feature generator

--- utils/extract_features.py
+++ utils/extract_features.py
@@ -6,17 +6,24 @@ import itertools
 import pickle
 
 def generate(dir, disp=False):
-    dir = os.fsencode(curr_dir)
+    dir = os.fsencode(sys.argv[1])
     data = []
-    numFiles = len(os.listdir(curr_dir))
+    numFiles = len(os.listdir(sys.argv[1]))
     for num, file in enumerate(os.listdir(dir)):
         fname = os.fsencode(file)
-        
+
         y, sr = librosa.load(dir+fname)
         data.append(librosa.feature.mfcc(y=y, sr=sr))
-        print('processing wavs '+str(round(100*(num+1)/numFiles, 2))+'%', end='\r')
-    print('')
-    return data
+        if disp: print('processing wavs '+str(round(100*(num+1)/numFiles, 2))+'%', end='\r')
+
+    if disp: print('padding... ', end='')
+    maxRows = max([elem.shape[0] for elem in data])
+    maxCols = max([elem.shape[1] for elem in data])
+    padded = [np.zeros((maxRows, maxCols)) for _ in data]
+    for ins, zer in zip(data, padded): zer[:ins.shape[0], :ins.shape[1]] = ins
+    if disp: print('done')
+   
+    return padded
 
 
 

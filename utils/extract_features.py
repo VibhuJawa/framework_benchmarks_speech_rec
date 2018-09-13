46f7cb410dcfcfc0a38603f1e9db4adb4d054aec
vibhujawa <vibhujawa@gmail.com>
2018-09-12 21:49:51 -0400

    working code

--- utils/extract_features.py
+++ utils/extract_features.py
@@ -5,19 +5,21 @@ import librosa
 import itertools
 import pickle
 
-def generate(dir_name, disp=False):
-    dir = os.fsencode(dir_name)
+def generate(dir, disp=False):
+    dir = os.fsencode(curr_dir)
     data = []
-    numFiles = len(os.listdir(dir_name))
+    numFiles = len(os.listdir(curr_dir))
     for num, file in enumerate(os.listdir(dir)):
         fname = os.fsencode(file)
-
+        
         y, sr = librosa.load(dir+fname)
-        data.append(list(itertools.chain.from_iterable(librosa.feature.mfcc(y=y, sr=sr))))
+        data.append(librosa.feature.mfcc(y=y, sr=sr))
         print('processing wavs '+str(round(100*(num+1)/numFiles, 2))+'%', end='\r')
     print('')
-    length = min([len(elem) for elem in data])
-    return numpy.array([numpy.array(l[:length]) for l in data])
+    return data
+
+
+
 
 if '__main__' == __name__:
     if len(sys.argv)<3:
@@ -26,7 +28,6 @@ if '__main__' == __name__:
     input_dir = sys.argv[1]
     target_dir = sys.argv[2]
     os.makedirs(target_dir,exist_ok=True)
-    print(input_dir,target_dir)
     for label in os.listdir(input_dir):
         if label == '.DS_Store':
             continue

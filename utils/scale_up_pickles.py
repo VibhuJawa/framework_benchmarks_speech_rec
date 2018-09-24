22ae614fbe76954eb9c198b96422f1c51bdf756d
vibhujawa <vibhujawa@gmail.com>
2018-09-23 20:32:57 -0400

    added scale up code

---
+++ utils/scale_up_pickles.py
@@ -0,0 +1,11 @@
+import os
+import pickle
+
+data_dir = "../data/pickles"
+scaleup = 10 
+files = os.listdir(data_dir)
+for file in files:
+    pickled_file = pickle.load(open("{}/{}".format(data_dir,file),"rb"))
+    pickled_file = pickled_file * scaleup
+    print("Len now is {}".format(pickled_file ))
+    pickle.dump( pickled_file, open( "{}/{}".format(data_dir,file), "wb" ) )
